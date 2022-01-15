d718b8e3e97b88c4f16572df0b5d55dc053293e6
Elijah Schrock <70502486+ElijahSchrock@users.noreply.github.com>
2022-01-15 11:28:17 -0800

    added css

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 require('dotenv').config()
 const express = require('express');
+const { get } = require('express/lib/response');
+const res = require('express/lib/response');
 
 const app = express();
 
+//Express Settings
+app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
+app.use(express.static('public'));
 
+//Controllers & Routes
 app.use('/places', require('./controllers/places'));
 
 app.get('/', (req, res) => {
@@ -16,7 +22,7 @@ app.get('*', (req, res) => {
     res.render('error404')
 })
 
-
+//Listen for PORT conenections
 app.listen(process.env.PORT, () => {
     console.log(`Awoken and landed on PORT ${process.env.PORT}`);
 })
